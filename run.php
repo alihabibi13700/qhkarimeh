b934220f7e2e363ed842b445bd58645043a3e6ef
ali <alihabibi13700@gmail.com>
2019-08-22 16:15:41 -0700

    add qasemi page

--- run.php
+++ run.php
@@ -1,4 +1,25 @@
 <?php 
-echo 10*5;
+if(isset($_GET['file']))
+{
+	$file = file_get_contents($_GET['file']);
+	require_once('default.php');
+}
+else
+{
 
- ?>
+
+	$page = glob(__DIR__. "/page/*");
+	$allPage = [];
+
+	foreach ($page as $key => $value) 
+	{
+		$myList = glob($value. '/*');
+		$allPage = array_merge($allPage, $myList);
+	}
+	foreach ($allPage as $key => $value) {
+		
+		echo "<a href='http://localhost/qhkarimeh/run.php?file=$value'>$value</a><br>";
+	}
+
+}
+?>
