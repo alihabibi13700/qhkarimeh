a8b7e844807c78954d21caaeea4a90007f570c0e
ali <alihabibi13700@gmail.com>
2019-08-22 16:18:11 -0700

    add default page

---
+++ default.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl" prefix="og: http://ogp.me/ns#">
+<head>
+ <meta charset="UTF-8"/>
+ <base href="https://qhkarimeh.ir"/>
+ <title>همایش قرآن و ولایت (ویژه عید غدیرخم) | مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام</title>
+ <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><![endif]-->
+ <!--[if lte IE 9]><script>document.location = 'https://deadbrowser.com/fa';</script><![endif]-->
+ <meta name     ="description"             content="همایش بزرگ قرآن و ولایت توسط واحد آموزش و تربیت برادران مرکز قرآن و حدیث کریمه اهل بیت علیها السلام، روز یکشنبه ۲۷ مردادماه ۱۳۹۸ در شبستان حضرت نجمه خاتون (علیها السلام) برگزار گردید."/>
+ <meta name     ="site:root"               content="https://qhkarimeh.ir"/>
+ <meta name     ="twitter:card"            content="summary_large_image"/>
+ <meta name     ="twitter:site"            content="@ermile_azvir"/>
+ <meta name     ="twitter:title"           content="همایش قرآن و ولایت (ویژه عید غدیرخم) | مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام"/>
+ <meta name     ="twitter:description"     content="همایش بزرگ قرآن و ولایت توسط واحد آموزش و تربیت برادران مرکز قرآن و حدیث کریمه اهل بیت علیها السلام، روز یکشنبه ۲۷ مردادماه ۱۳۹۸ در شبستان حضرت نجمه خاتون (علیها السلام) برگزار گردید."/>
+ <meta name     ="twitter:url"             content="https://qhkarimeh.ir/quran-va-velayat"/>
+ <meta name     ="twitter:image"           content="https://qhkarimeh.ir/files/2/114-805e9a007d92c7c5112823229211c1be.jpg"/>
+ <meta property ="og:title"                content="همایش قرآن و ولایت (ویژه عید غدیرخم) | مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام"/>
+ <meta property ="og:description"          content="همایش بزرگ قرآن و ولایت توسط واحد آموزش و تربیت برادران مرکز قرآن و حدیث کریمه اهل بیت علیها السلام، روز یکشنبه ۲۷ مردادماه ۱۳۹۸ در شبستان حضرت نجمه خاتون (علیها السلام) برگزار گردید."/>
+ <meta property ="og:url"                  content="https://qhkarimeh.ir/quran-va-velayat"/>
+ <meta property ="og:type"                 content="website"/>
+ <meta property ='og:locale'               content='fa'/>
+ <meta property ="og:image"                content="https://qhkarimeh.ir/files/2/114-805e9a007d92c7c5112823229211c1be.jpg"/>
+ <meta name     ="viewport"                content="width=device-width, initial-scale=1.0, height=device-height, minimum-scale=1.0, maximum-scale=1.1, user-scalable=0"/>
+ <meta name     ="application-name"        content="مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام"/>
+ <meta name     ="msapplication-config"    content="https://qhkarimeh.ir/static/images/favicons/browserconfig.xml?v=1">
+ <meta name     ="theme-color"             content="#ffffff">
+ <link rel      ="apple-touch-icon"        href="https://qhkarimeh.ir/static/images/favicons/apple-touch-icon.png?v=1" sizes="180x180">
+ <link rel      ="icon"                    href="https://qhkarimeh.ir/static/images/favicons/favicon-32x32.png?v=1" sizes="32x32" type="image/png">
+ <link rel      ="icon"                    href="https://qhkarimeh.ir/static/images/favicons/favicon-16x16.png?v=1" sizes="16x16" type="image/png">
+ <link rel      ="mask-icon"               href="https://qhkarimeh.ir/static/images/favicons/safari-pinned-tab.svg?v=1">
+ <link rel      ="shortcut icon"           href="https://qhkarimeh.ir/static/images/favicons/favicon.ico?v=1">
+ <link rel      ="manifest"                href="https://qhkarimeh.ir/manifest.webmanifest">
+ <link rel="author"           href="https://qhkarimeh.ir/humans.txt"/>
+
+ <link rel="stylesheet"       href="https://qhkarimeh.ir/static/siftal/css/siftal.css?1566130497"/>
+ <link rel="stylesheet"       href="https://qhkarimeh.ir/enterprise/qhkarimeh/css/qhkarimeh.css?"/>
+</head>
+
+<body data-in='site' data-page='quran-va-velayat' class='rtl preload ' data-user='5'>
+<section id='landing' data-scroll>
+  <div class="bg"></div>
+  <div id="nodes"></div>
+  <header>
+    <h2 class="s0">آستان مقدس حضرت فاطمه معصومه سلام الله علیها</h2>
+    <h1><a href="https://qhkarimeh.ir">مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام</a></h1>
+    <a href="https://qhkarimeh.ir/a" data-direct class="btn lg success mA5">ورود به سامانه آموزشی</a>
+    <a href="https://qhkarimeh.ir/support" data-direct class="btn lg primary mA5"><span class="sf-question-circle"></span> مرکز راهنمایی</a>
+    <div class="mT10">
+      </div>
+  </header>
+</section> <div class="wrapper" data-xhr='wrapper'>
+  <div class="pc">
+   <div class="cn">
+
+<div class="blogEx box">
+  <article>
+  <?php echo $file; ?>
+  
+</article>
+
+</div>
+
+   </div>
+  </div>
+ </div>
+<section class='touchBox' id='contactus'>
+  <h3>با ما در تماس باشید</h3>
+  <address>
+    <p class="fs06 mB50-f fc-mute">قم، آستان مقدس حضرت فاطمه معصومه سلام الله علیها، مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام</p>
+    <div class="f mB50">
+      <div class="c6 s12">
+        <div class="label">آموزش قرآن برادران</div>
+        <div class="value ltr"><a href="tel:+982537175527">۰۲۵ ۳۷۱۷ ۵۵۲۷</a></div>
+      </div>
+      <div class="c6 s12">
+        <div class="label">آموزش غیرحضوری</div>
+        <div class="value ltr"><a href="tel:+982537175530">۰۲۵ ۳۷۱۷ ۵۵۳۰</a></div>
+      </div>
+    </div>
+    <div class="f mB50">
+      <div class="c6 s12">
+        <div class="label">آموزش معارف برادران</div>
+        <div class="value ltr"><a href="tel:+982537175520">۰۲۵ ۳۷۱۷ ۵۵۲۰</a></div>
+      </div>
+      <div class="c6 s12">
+        <div class="label">آموزش قرآن خواهران</div>
+        <div class="value ltr"><a href="tel:+982537175505">۰۲۵ ۳۷۱۷ ۵۵۰۵</a></div>
+      </div>
+    </div>
+    <div class="f">
+      <div class="c6 s12">
+        <div class="label">شعبه شهدا (صفائیه)</div>
+        <div class="value ltr"><a href="tel:+982537744808">۰۲۵ ۳۷۷۴ ۴۸۰۸</a></div>
+      </div>
+      <div class="c6 s12">
+        <div class="label">آموزش معارف خواهران</div>
+        <div class="value ltr"><a href="tel:+982537175515">۰۲۵ ۳۷۱۷ ۵۵۱۵</a></div>
+      </div>
+    </div>
+  </address>
+</section>
+
+
+<footer>
+  <div class="cn">
+    <h2><a href="https://qhkarimeh.ir">مرکز قرآن و حدیث کریمه اهل‌ بیت علیها السلام</a></h2>
+    <h3>هر لحظه با قرآن باش</h3>
+    <nav class="langlist" data-xhr="langlist" data-url=""><a href="https://qhkarimeh.ir/en" hreflang="en" data-direct>English</a></nav>
+    <nav class="share1">
+  <a data-direct href="https://t.me/qhkarimeh" class="telegram">qhkarimeh telegram</a>
+  <a data-direct href="https://instagram.com/qhkarimeh" class="instagram">qhkarimeh instagram</a>
+</nav>
+   <nav class="top">
+    <a class="link_home" href="https://qhkarimeh.ir">صفحه نخست</a>
+    <a href="https://qhkarimeh.ir/about">درباره</a>
+    <a href="https://qhkarimeh.ir/contact">تماس</a>
+    <a href="https://qhkarimeh.ir/pricing">هزینه‌ها</a>
+    <a href="https://qhkarimeh.ir/support/faq" data-direct>سوالات پرتکرار</a>
+    <a href="https://qhkarimeh.ir/elearn" data-direct>آموزش غیرحضوری و مجازی</a>
+   </nav>
+
+   <div class="f middle">
+      <a class="c s6" href="https://qhkarimeh.ir/terms">شرایط استفاده</a>
+      <a class="c s6" href="https://qhkarimeh.ir/privacy">حریم خصوصی</a>
+      <a class="c s6" href="https://qhkarimeh.ir/socialresponsibility">مسئولیت اجتماعی</a>
+      <div class="c12 s6 love"><a href="https://azvir.com/fa" target="_blank" title='کاری نداره شعار ماست!&lt;br&gt;اگر راه‌کاری مناسب برای مشکل خود نیافته‌اید، با ما تماس بگیرید.'>ساخته شده با ازویر</a></div>
+   </div>
+
+
+  </div>
+</footer>
+
+  <div id="xaccess">
+    <a class='siteAdmin public' data-direct href='https://qhkarimeh.ir/cms' title='کنترل پنل' data-tippy-placement='right' data-tippy-animation="shift-away" data-tippy-inertia="true"><span class="sf-gauge"></span></a>
+  </div>
+ <script src="https://qhkarimeh.ir/static/siftal/js/siftal.min.js?1566130497"></script>
+<noscript>
+ <div class="line top danger fs16">
+  <div class="txtB">جاوا اسکریپت برای استفاده از سرویس ما ضروری است.</div>
+  جاوا اسکریپت را در مرورگر خود فعال کنید یا از مرورگری استفاده کنید که  آن پشتیبانی می‌کند. </div>
+</noscript>
+
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130958695-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-130958695-3');
+
+  function pushStateGA()
+  {
+    var origin = window.location.protocol + '//' + window.location.host;
+    var pathname = window.location.href.substr(origin.length);
+    gtag('config', 'UA-130958695-3', {'page_path': pathname});
+  }
+</script>
+
+ <div data-xhr='foot_js' class="foot_js">
+<script src="https://qhkarimeh.ir/enterprise/qhkarimeh/js/qhkarimeh.js?"></script>
+</div>
+</body>
+</html>
