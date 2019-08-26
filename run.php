ee849497d68a1c02cc383ad3dd9f286cd91f131f
Ali Habibi <alihabibi13700@gmail.com>
2019-08-25 22:15:50 -0700

    fix load run.php

--- run.php
+++ run.php
@@ -6,19 +6,60 @@ if(isset($_GET['file']))
 }
 else
 {
-
-
-	$page = glob(__DIR__. "/page/*");
 	$allPage = [];
 
-	foreach ($page as $key => $value) 
+	$folder = ['page', 'news'];
+
+	foreach ($folder as  $folder_name) 
 	{
-		$myList = glob($value. '/*');
-		$allPage = array_merge($allPage, $myList);
+		$page = glob(__DIR__. "/$folder_name/*");
+
+		foreach ($page as $key => $value) 
+		{
+			$myList = glob($value. '/*');
+			$allPage = array_merge($allPage, $myList);
+		}
 	}
-	foreach ($allPage as $key => $value) {
+
+	$show = [];
+	foreach ($allPage as $key => $value) 
+	{
+		$name = basename($value);
+		$folderName = str_replace('/'.$name, '', $value);
+		$folderName = explode('/', $folderName);
+		$endFoldr = end($folderName);
+		array_pop($folderName);
+		$masterFolder = end($folderName);
 		
-		echo "<a href='http://localhost/qhkarimeh/run.php?file=$value'>$value</a><br>";
+		if(!isset($show[$masterFolder]))
+		{
+			$show[$masterFolder] = [];
+		}
+
+		if(!isset($show[$masterFolder][$endFoldr]))
+		{
+			$show[$masterFolder][$endFoldr] = [];
+		}
+
+		$show[$masterFolder][$endFoldr][] = ['name' => $name, 'folder' => $endFoldr, 'addr' => $value];
+
+	}
+
+	foreach ($show as $key => $value) 
+	{
+		echo "<h2>$key</h2>";
+		echo "<hr>";
+		foreach ($value as $k => $v) 
+		{
+			echo "<hr>";
+			echo "<h4>$k</h4>";
+			foreach ($v as $a => $b) 
+			{
+				echo "<a href='http://localhost/qhkarimeh/run.php?file=$b[addr]'>$b[name]</a><br>";
+			}
+			
+		}
+	
 	}
 
 }
